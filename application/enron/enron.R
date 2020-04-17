3de939796359f9125648eb72859f6b0cc3c6e7db
ludkinm <m.ludkin1@lancaster.ac.uk>
2020-04-17 17:27:26 +0100

    applications edit plot labels

--- application/enron/enron.R
+++ application/enron/enron.R
@@ -1,6 +1,4 @@
-library(coda)
 library(ggplot2)
-library(reshape2)
 library(SBMSplitMerge)
 
 thm <- ggplot2::theme_gray(base_size = 20)
@@ -24,8 +22,8 @@ sigma <- 0.5
     )
     NegBinDMA$rj_output <- sampler(Enron$Edges, NegBinDMA, n_steps, "rj", sigma)
     NegBinDMA$plots <- eval_plots(NegBinDMA$rj_output)
+    save(NegBinDMA, n_steps, sigma, file="rj_nbin.RData")
 }
-
 
 {
     PoisDMA <- list(
@@ -35,65 +33,62 @@ sigma <- 0.5
     )
     PoisDMA$rj_output <- sampler(Enron$Edges, PoisDMA, n_steps, "rj", sigma)
     PoisDMA$plots <- eval_plots(PoisDMA$rj_output)
+    save(PoisDMA, n_steps, sigma, file="rj_pois.RData")
 }
 
+load("rj_nbin.RData")
 {
-    ggsave("rj_nbin_num_blocks_trace.pdf"    , NegBinDMA$plots$num_blocks_trace + thm + ylab("K"))
-    ggsave("rj_nbin_blocks_trace.pdf"        , NegBinDMA$plots$blocks_trace + thm)
-    ggsave("rj_nbin_blocks_trace_sorted.pdf" , NegBinDMA$plots$blocks_trace_sorted + thm)
-    ggsave("rj_nbin_post_pairs.pdf"          , NegBinDMA$plots$post_pairs + thm + xlab("Node") + ylab("Node"))
-    ggsave("rj_nbin_post_pairs_sorted.pdf"   , NegBinDMA$plots$post_pairs_sorted + thm + xlab("Node") + ylab("Node"))
-
+    ## convert edges to log scale and plot sorted by the assignment computed in eval_plots
     nbin_le <- log(Enron$Edges$E[NegBinDMA$plots$sortind,NegBinDMA$plots$sortind])
     nbin_le <- ifelse(nbin_le > 0, log(nbin_le), 0)
-    image(edges(nbin_le))
 
-    ggsave("rj_nbin_log_edges_sorted.pdf", image(edges(nbin_le)) + thm + xlab("Node") + ylab("Node"))
+    ggsave("rj_nbin_blocks_trace.pdf"        , NegBinDMA$plots$blocks_trace + thm)
+    ggsave("rj_nbin_blocks_trace_sorted.pdf" , NegBinDMA$plots$blocks_trace_sorted + thm)
+    ggsave("rj_nbin_num_blocks_trace.pdf"    , NegBinDMA$plots$num_blocks_trace + thm)
+    ggsave("rj_nbin_post_pairs.pdf"          , NegBinDMA$plots$post_pairs + thm)
+    ggsave("rj_nbin_post_pairs_sorted.pdf"   , NegBinDMA$plots$post_pairs_sorted + thm)
+    ggsave("rj_nbin_log_edges_sorted.pdf"    , image(edges(nbin_le)) + thm)
 
     burn <- (NegBinDMA$rj_output$nsteps/2):NegBinDMA$rj_output$nsteps
     inds <- 1:8
     thetas1 <- t(NegBinDMA$rj_output$postt[1,inds,burn])
     thetas2 <- t(NegBinDMA$rj_output$postt[2,inds,burn])
 
-    ## tables
-    signif(cbind(t(apply(thetas1, 2, quantile, c(0.05, 0.5, 0.95))), effectiveSize(thetas1)), 2)
-    signif(cbind(t(apply(thetas2, 2, quantile, c(0.05, 0.5, 0.95))), effectiveSize(thetas2)), 2)
+    ## table of metrics
+    signif(cbind(t(apply(thetas1, 2, quantile, c(0.05, 0.5, 0.95))), coda::effectiveSize(thetas1)), 2)
+    signif(cbind(t(apply(thetas2, 2, quantile, c(0.05, 0.5, 0.95))), coda::effectiveSize(thetas2)), 2)
 
     df <- data.frame(cbind(burn, thetas1))
     names(df) <- c("index", paste0("theta1_", 1:ncol(thetas1)-1))
-    mf <- melt(df, id="index")
+    mf <- reshape2::melt(df, id="index")
     ggsave("rj_nbin_theta1.pdf", ggplot(mf, aes(x=index, y=value, col=variable)) + geom_line()+ thm)
 
     df <- data.frame(cbind(burn, thetas2))
     names(df) <- c("index", paste0("theta2_", 1:ncol(thetas2)-1))
-    mf <- melt(df, id="index")
+    mf <- reshape2::melt(df, id="index")
     ggsave("rj_nbin_theta2.pdf", ggplot(mf, aes(x=index, y=value, col=variable)) + geom_line()+ thm)
-
-    save(NegBinDMA, n_steps, sigma, file="rj_nbin.RData")
 }
 
+
+load("rj_pois.RData")
 {
-    ggsave("rj_pois_num_blocks_trace.pdf"    , PoisDMA$plots$num_blocks_trace + thm + ylab("K"))
     ggsave("rj_pois_blocks_trace.pdf"        , PoisDMA$plots$blocks_trace + thm)
     ggsave("rj_pois_blocks_trace_sorted.pdf" , PoisDMA$plots$blocks_trace_sorted + thm)
-    ggsave("rj_pois_post_pairs.pdf"          , PoisDMA$plots$post_pairs + thm + xlab("Node") + ylab("Node"))
-    ggsave("rj_pois_post_pairs_sorted.pdf"   , PoisDMA$plots$post_pairs_sorted + thm + xlab("Node") + ylab("Node"))
+    ggsave("rj_pois_num_blocks_trace.pdf"    , PoisDMA$plots$num_blocks_trace + thm)
+    ggsave("rj_pois_post_pairs.pdf"          , PoisDMA$plots$post_pairs + thm)
+    ggsave("rj_pois_post_pairs_sorted.pdf"   , PoisDMA$plots$post_pairs_sorted + thm)
 
     pois_le <- log(Enron$Edges$E[PoisDMA$plots$sortind,PoisDMA$plots$sortind])
     pois_le <- ifelse(pois_le > 0, log(pois_le), 0)
-    image(edges(pois_le))
-
-    ggsave("rj_pois_log_edges_sorted.pdf", image(edges(pois_le)) + thm + xlab("Node") + ylab("Node"))
+    ggsave("rj_pois_log_edges_sorted.pdf", image(edges(pois_le)) + thm)
 
     burn <- (PoisDMA$rj_output$nsteps/2):PoisDMA$rj_output$nsteps
     inds <- 1:8
     thetas <- t(PoisDMA$rj_output$postt[1,inds,burn])
-    signif(cbind(t(apply(thetas, 2, quantile, c(0.05, 0.5, 0.95))), effectiveSize(thetas)), 2)
+    signif(cbind(t(apply(thetas, 2, quantile, c(0.05, 0.5, 0.95))), coda::effectiveSize(thetas)), 2)
 
     df <- data.frame(cbind(burn, thetas))
     names(df) <- c("index", paste0("theta1_", 1:ncol(thetas)-1))
-    mf <- melt(df, id="index")
+    mf <- reshape2::melt(df, id="index")
     ggsave("rj_pois_theta1.pdf", ggplot(mf, aes(x=index, y=value, col=variable)) + geom_line() + thm)
-
-    save(PoisDMA, n_steps, sigma, file="rj_pois.RData")
 }
--- application/macaque/macaque.R
+++ application/macaque/macaque.R
@@ -1,8 +1,5 @@
-library(coda)
-library(reshape2)
 library(ggplot2)
 library(SBMSplitMerge)
-library(clue)
 
 thm <- ggplot2::theme_gray(base_size = 20)
 set.seed(1)
@@ -14,35 +11,34 @@ ggsave("edges.pdf", image(Macaque) + thm  + xlab("Node") + ylab("Node"))
 DMA <- list(edges = edges_bern(), params = param_beta(1,1,1,1), blocks = dma(1, 6))
 
 n_steps <- 15000
+n_burn  <- 1000
+burnin  <- n_burn:n_steps
 sigma   <- 0.1
 
 DMA$output <- sampler(Macaque, DMA, n_steps, "rj", sigma)
+DMA$plots  <- eval_plots(DMA$output, burnin)
+save(DMA, file="rj_output.RData")
 
-burn <- 1000:15000
-DMA$plots  <- eval_plots(DMA$output, burn=burn)
+load("rj_output.RData")
+ggsave("rj_blocks_trace.pdf"        , DMA$plots$blocks_trace + thm)
+ggsave("rj_blocks_trace_sorted.pdf" , DMA$plots$blocks_trace_sorted + thm)
+ggsave("rj_num_blocks_trace.pdf"    , DMA$plots$num_blocks_trace + thm)
+ggsave("rj_post_pairs.pdf"          , DMA$plots$post_pairs + thm)
+ggsave("rj_post_pairs_sorted.pdf"   , DMA$plots$post_pairs_sorted + thm)
+ggsave("rj_edges_sorted.pdf"        , image(edges(Macaque$E[DMA$plots$sortind,DMA$plots$sortind])) + thm)
 
-ggsave("rj_num_blocks_trace.pdf", DMA$plots$num_blocks_trace + thm + ylab("K"))
-ggsave("rj_blocks_trace.pdf", DMA$plots$blocks_trace + thm)
-ggsave("rj_blocks_trace_sorted.pdf", DMA$plots$blocks_trace_sorted + thm)
-ggsave("rj_post_pairs.pdf", DMA$plots$post_pairs + thm + xlab("Node") + ylab("Node"))
-ggsave("rj_post_pairs_sorted.pdf", DMA$plots$post_pairs_sorted + thm + xlab("Node") + ylab("Node"))
-ggsave("rj_edges_sorted.pdf", image(edges(Macaque$E[DMA$plots$sortind,DMA$plots$sortind])) + thm+ xlab("Node") + ylab("Node"))
-
-thetas <- t(DMA$output$postt[1,1:5,burn])
+thetas <- t(DMA$output$postt[1,1:5,burnin])
 
 ## table for latex
-DMA$theta_table <- signif(cbind(t(apply(thetas, 2, quantile, probs=c(0.05, 0.5, 0.95))), effectiveSize(thetas)), 2)
+DMA$theta_table <- signif(cbind(t(apply(thetas, 2, quantile, probs=c(0.05, 0.5, 0.95))), coda::effectiveSize(thetas)), 2)
 colnames(DMA$theta_table) <- c("5%", "50%", "95%", "Effective Size")
 rownames(DMA$theta_table) <- paste("theta_", 1:5 -1)
-
 DMA$theta_table
 
 ## acceptance
 colMeans(diff(thetas) == 0.0)
 
-df <- data.frame(cbind(burn, thetas))
+df <- data.frame(cbind(burnin, thetas))
 names(df) <- c("index", paste0("theta", 1:ncol(thetas)-1))
-mf <- melt(df, id="index")
+mf <- reshape2::melt(df, id="index")
 ggsave("rj_theta.pdf", ggplot(mf, aes(x=index, y=value, col=variable)) + geom_line() + ylim(c(0,1)) + thm)
-
-save(DMA, file="rj_output.RData")
